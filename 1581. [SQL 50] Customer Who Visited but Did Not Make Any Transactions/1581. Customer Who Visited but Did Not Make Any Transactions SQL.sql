f1f0eadce892ef6ca8fc94e96c4d914388e171bb
Harshkumarguptagit <harshkumargupta49@gmail.com>
2026-06-15 14:49:56 +0530

    1581. [SQL 50] Customer Who Visited but Did Not Make Any Transactions

---
+++ 1581. [SQL 50] Customer Who Visited but Did Not Make Any Transactions/1581. Customer Who Visited but Did Not Make Any Transactions SQL.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    customer_id,
+    COUNT(visit_id) AS count_no_trans
+FROM visits
+WHERE visit_id NOT IN (
+    select visit_id from transactions
+)
+GROUP BY customer_id
